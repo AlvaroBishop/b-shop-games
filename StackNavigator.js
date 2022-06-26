9e26a2b3f3dcadf61a631e7eefa92a913f46a8a3
AlvaroBishop <bishopgamma@gmail.com>
2022-06-26 15:19:49 -0500

    agregado guessyournumber

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,11 +4,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Store from "./src/screens/Store";
 import GuessMyNumber  from "./src/screens/GuessMyNumber";
+import GuessYourNumber  from "./src/screens/GuessYourNumber";
+import { ChooseYourGameScreen } from "./src/screens/ChooseYourGameScreen";
 import PPoT from "./src/screens/PPoT";
+
 const Stack = createNativeStackNavigator();
 
 
-const DetailStackNavigator = () => {
+const MainStackNavigator = () => {
     return(
         <Stack.Navigator
             screenOptions={{
@@ -22,10 +25,29 @@ const DetailStackNavigator = () => {
             }}
         >
             <Stack.Screen name="Store" component={Store} />
-            <Stack.Screen name="GuessMyNumber" component={GuessMyNumber}  />
+            <Stack.Screen name="GuessNumbers" component={GuessStackNavigator}  options={{ headerShown: false}}/>
             <Stack.Screen name="PPoT" component={PPoT}  />
         </Stack.Navigator>
-        )
-    }
+    )
+}
 
-export { DetailStackNavigator };
+const GuessStackNavigator = () => {
+    return(
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: "black",
+                    borderColor: "#7ef7bd"
+                  },
+                  headerTintColor: "#7ef7bd",
+                  headerBackTitle: "Back",
+                  headerTitleAlign:'center'
+            }}
+        >
+            <Stack.Screen name="ChooseYourGame" component={ChooseYourGameScreen}  />
+            <Stack.Screen name="GuessMyNumber" component={GuessMyNumber}  />
+            <Stack.Screen name="GuessYourNumber" component={GuessYourNumber}  />
+        </Stack.Navigator>
+    )
+}
+export { MainStackNavigator };
--- src/screens/ChooseYourGameScreen.js
+++ src/screens/ChooseYourGameScreen.js
@@ -1,26 +1,26 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 
 export const ChooseYourGameScreen = () => {
     const navigation = useNavigation();
   return (
-    <View>
-        <View>
+    <View style={styles.game}>
+        <View >
             <TouchableOpacity
                 /* style={styles.buttonLocation} */
                 onPress={() => navigation.push('GuessMyNumber')}
             >
-                <View /* style={styles.button} */>
-                    <Text /* style={styles.buttonText} */>GuessMyNumber</Text>
+                <View style={styles.button}>
+                    <Text style={styles.winText}>GuessMyNumber</Text>
                 </View>
             </TouchableOpacity>
             <TouchableOpacity
                 /* style={styles.buttonLocation} */
                 onPress={() => navigation.push('GuessYourNumber')}
             >
-                <View /* style={styles.button} */>
-                    <Text /* style={styles.buttonText} */>GuessYourNumber</Text>
+                <View style={styles.button}>
+                    <Text style={styles.winText}>GuessYourNumber</Text>
                 </View>
             </TouchableOpacity>
 
@@ -28,3 +28,43 @@ export const ChooseYourGameScreen = () => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    game: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'black',
+      borderTopColor: "#7ef7bd", 
+      borderTopWidth: 1,
+    },
+    gameContainer: {
+        alignItems: 'center',
+        width: '100%',
+    },
+    buttonsContainer: {
+        backgroundColor: '#7ef7bd',
+        borderRadius: 20
+    },
+    winText: {
+      color: 'black', 
+      fontSize: 20,
+      marginVertical: 20,
+    },
+    input: {
+      textAlign: 'center',
+      margin: 35,
+      color: '#7ef7bd',
+      fontSize: 20,
+    },
+    listContainer: {
+        width: '100%',
+    },
+    button: {
+      padding: 10,
+      margin: 10,
+      backgroundColor: '#7ef7bd',
+      borderRadius: 200,
+    }
+   
+  })
